d442700844f54b85bae152f495bdadc8f48684c5
Chase Marshall <tdchase34@gmail.com>
2022-06-12 22:59:00 -0400

    Implemented Navbar

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Navbar from './components/Navbar';
 import Home from './Pages/Home';
 import ExerciseDetail from './Pages/ExerciseDetail';
 import Footer from './components/Footer';
+
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width: { xl: '1488px'}}} m="auto">
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
